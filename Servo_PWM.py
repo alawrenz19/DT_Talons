3798ed4f9b7b7968ec541563cf5aa1c0371ff787
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2018-12-13 10:27:13 -0600

    Update Servo_PWM.py

--- Servo_PWM.py
+++ Servo_PWM.py
@@ -4,6 +4,7 @@ RPL.init()
 f_reverse = 1000
 neutral = 1500
 f_forward = 2000
+# anglepersecond = 
 
 ServoR = int(raw_input("S_R > "))
 ServoL = int(raw_input("S_L > "))
@@ -11,9 +12,22 @@ ServoL = int(raw_input("S_L > "))
 RPL.pinMode(ServoR, RPL.PWM)
 RPL.pinMode(ServoL, RPL.PWM)
 
-
-
-
+def turn():
+  degreeturn = int(raw_input("Deg_RT > "))
+  currenttime = time.time()
+  if degreeturn > 90
+    TT = (degreeturn - 90) / anglepersecond
+    while time.time() < currenttime + TT:
+      RPL.pwmWrite(0, 2000, 3000) 
+    if time.time() > currenttime + T:
+      RPL.pwmWrite(0, 1500, 3000)
+  if degreeturn < 90
+    TT = degreeturn / anglepersecond
+    while time.time() < currenttime + TT:
+      RPL.pwmWrite(0, 1000, 3000)
+    if time.time() > currenttime + T:
+      RPL.pwmWrite(0, 1500, 3000)
+    
 
 
 
