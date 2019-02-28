7ee562bb7894b60c5dde77571f76d9fef82d04a6
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2019-02-28 11:00:06 -0600

    Update Servo_PWM.py

--- Servo_PWM.py
+++ Servo_PWM.py
@@ -10,9 +10,9 @@ f_forward = 2000
 def DT_PWM_Establish():
 	TalonR = int(raw_input("Pls input what pin \
 	you've inserted your right talon into >")
+	RPL.pinMode(ServoR, RPL.PWM)
 	TalonL = int(raw_input("Pls input what pin \
 	you've inserted your left talon into >")
-	RPL.pinMode(ServoR, RPL.PWM)
 	RPL.pinMode(ServoL, RPL.PWM)
 
 def DT_Speed():
