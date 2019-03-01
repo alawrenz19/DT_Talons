c74fe14ac9e998b3aa4d33fe33ca1b10a548af20
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2019-03-01 10:34:21 -0600

    Update Servo_PWM.py

--- Servo_PWM.py
+++ Servo_PWM.py
@@ -18,15 +18,15 @@ def DT_PWM_Establish():
 def DT_Speed():
 	DT_input = int(raw_input("Pls input what speed you want to run the motors at: 4 = Forward_fastest, \
 	3 = Foward_fast, 0 = stop, 2 = Reverse_fast, 1 = Reverse_fastest > "))
-	if DT_input = 4:
+	if DT_input == 4:
 		RPL.pwmWrite(0, 2000, 3000) 
-	if DT_input = 3:
+	if DT_input == 3:
 		RPL.pwmWrite(0, 1750, 3000) 
-	if DT_input = 0:
+	if DT_input == 0:
 		RPL.pwmWrite(0, 1500, 3000) 
-	if DT_input = 2:
+	if DT_input == 2:
 		RPL.pwmWrite(0, 1250, 3000) 
-	if DT_input = 1:
+	if DT_input == 1:
 		RPL.pwmWrite(0, 1000, 3000) 
 	else: print("The number you inputted is not an available option. Pls run the code again with one of the 5 numbers.")
 
