69dc108dec0b4ca0ae0d374ead20b3e7d8f2f601
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2019-03-07 11:20:45 -0600

    Update Calibration.py

--- Calibration.py
+++ Calibration.py
@@ -9,7 +9,7 @@ f_forward = 2000
 
 def DT_Servo_Calibration():
   Talon_Number = int(raw_input("Would you like to calibrate one or two talons? Pls print either '1' or '2' > "))
-  If Talon_Number = 2:
+  if Talon_Number == 2:
 	ServoR = int(raw_input("Pls input what pin you've inserted your right talon into > "))
 	RPL.pinMode(ServoR, RPL.PWM)
 	ServoL = int(raw_input("Pls input what pin you've inserted your left talon into > "))
@@ -26,7 +26,7 @@ def DT_Servo_Calibration():
     	RPL.pwmWrite(ServoL, 1500, 3000)
     	Print("Now, pls release the calibration button. If the talon was successfullu calibrated it should blink green.")
     
-  If Talon_Number == 1: 
+  if Talon_Number == 1: 
     	Servo_IVL = int(raw_input("Pls input what pin you've inserted your right talon into > "))
 	RPL.pinMode(Servo_IVL, RPL.PWM)
     	Print("Now, pls hold down the calibration button, which says 'B/C CAL'.")
