41ed3b00cbef318cf8004dac5679629b539e37c8
alawrenz19 <33064599+alawrenz19@users.noreply.github.com>
2019-05-16 10:52:14 -0500

    Update Servo_PWM.py

--- Servo_PWM.py
+++ Servo_PWM.py
@@ -29,7 +29,7 @@ def DT_PWM_Speedrange():
 	else:
 		print("The number you inputted is not an available option. Pls run the code again with one of the 5 numbers.")
 	
-	DT_input_L = int(raw_input("Pls input what speed you want to run the left motor at: 4 = Forward_fastest, 3 = Foward_fast, 0 = stop, 2 = Reverse_fast, 1 = Reverse_fastest > "))
+	DT_input_L = int(raw_input("Pls input what speed you want to run the right motor at: 4 = Forward_fastest, 3 = Foward_fast, 6 = Forward_veryslow,0 = stop, 2 = Reverse_fast, 1 = Reverse_fastest > "))
 	if DT_input_L == 4:
 		RPL.pwmWrite(ServoL, 2000, 3000) 
 	elif DT_input_L == 3:
